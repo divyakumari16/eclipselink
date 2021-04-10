5ddb1520868227735c0b9d52178f276aa96fcb9f
divya <divya@DESKTOP-JLGUNJD>
2021-04-10 10:43:45 +0530

    hey eclipse

---
+++ EclipseLink/src/EcGit.java
@@ -0,0 +1,17 @@
+
+public class EcGit {
+	int i;
+	String s;
+	EcGit()
+	{
+		 System.out.println("i: "+i);
+		 System.out.println("s: "+s);
+	}
+	public static void main(String[] main)
+	{
+		EcGit e=new EcGit();
+		
+	}
+	 
+}
+
